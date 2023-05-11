30133b07e656069ec25c70586a10d0f49d357dfa
Holly Nereson <hollyn@reddotstorage.com>
2023-05-11 12:54:14 -0600

    initial

---
+++ app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+from helpers import *
+import folium
+from streamlit_folium import folium_static 
+from folium.plugins import TimestampedGeoJson
+
+#---------------SETTINGS--------------------
+page_title = "Tenant Locations "
+page_icon = ":world-map:"  #https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+initial_sidebar_state="collapsed"
+#-------------------------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout, initial_sidebar_state=initial_sidebar_state)
+
+# --- HIDE STREAMLIT STYLE ---
+hide_st_style = """
+            <style>
+            MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# ----- FUNCTIONS -----
+def blank(): return st.write('') 
+
+tenants = run_sql_query(all_tenants)
+master_accounts = grab_pkl('rd-demographics', 'accounts/master_accounts.pkl')
+facilities = run_sql_query(facilities_data)
+
+list_rds = facilities['rd'].tolist()
+geocoded_rds = facilities[['rd', 'latitude', 'longitude']]
+
+st.title("Facility Locations")
+with st.form("entry_form", clear_on_submit=False):
+    col1, col2 = st.columns(2)
+    selected_rds = col1.multiselect("Select a RD:", list_rds, key=list)
+    
+    timeseries = st.checkbox("Do you want to see a time series?")
+    submitted = st.form_submit_button("Submit")
+    if timeseries:
+        current = False
+    else:
+        current = True 
+
+if submitted:
+    with st.spinner("Searching for tenants..."):
+
+        if selected_rds:  # Check if the list is not empty
+            first_geocoded_rds = geocoded_rds[geocoded_rds['rd'] == selected_rds[0]]
+        else:
+            st.warning("No RD selected. Please select at least one RD.")
+        m = folium.Map(location=[first_geocoded_rds["latitude"].iloc[0], first_geocoded_rds["longitude"].iloc[0]], zoom_start=11, tiles=None)
+
+        esri_street_map = folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Satellite',
+            overlay=False,
+            control=True
+        ).add_to(m)
+        
+        esri_street_map.add_to(m)
+
+        # Define tenant colors for each facility
+        tenant_colors = ["#34ECF4", "#f43c34","#8CF434", "#9C34F4", "#F49C34", "#F4348C", "#34F43C", "#3C34F4"]
+
+        # Call the specific_rds_geocoded function for each RD and concatenate the results
+        all_rd_geocoded = specific_rds_geocoded(tenants, selected_rds, current, master_accounts)
+
+        geocoded = all_rd_geocoded[['site_code', 'account_id', 'lat', 'lon']]
+
+        for index, rd in enumerate(selected_rds):
+            selected_geocoded_rds = geocoded_rds[geocoded_rds['rd'] == rd]
+
+            # Add custom image marker for the facility
+            icon = folium.CustomIcon(icon_image="rd_logo.png", icon_size=(35, 35))
+            folium.Marker(
+                location=[selected_geocoded_rds["latitude"].iloc[0], selected_geocoded_rds["longitude"].iloc[0]],
+                icon=icon,
+                popup=f"{rd}",
+            ).add_to(m)
+            
+            if timeseries == False:
+            # Filter tenant locations for the current facility
+                current_rd_geocoded = geocoded[geocoded['site_code'] == rd]
+
+            # Add scatter markers for tenant locations
+                for _, row in current_rd_geocoded.iterrows():
+                    folium.CircleMarker(
+                        location=[row["lat"], row["lon"]],
+                        radius=5,
+                        color=tenant_colors[index % len(tenant_colors)],
+                        fill=True,
+                        fill_color=tenant_colors[index % len(tenant_colors)],
+                        fill_opacity=0.7,
+                        popup=f"{row['site_code']} account {int(row['account_id'])}",
+                    ).add_to(m)
+            
+        if timeseries == False:
+            folium_static(m)
+        
+        if timeseries:
+            dated_dict = generate_date_dict(all_rd_geocoded)
+            # Assuming selected_rds is a list of RDs
+            rd_colors = {rd: tenant_colors[i % len(tenant_colors)] for i, rd in enumerate(selected_rds)}
+            geojson_data = dict_to_geojson(dated_dict,rd_colors )
+            
+            TimestampedGeoJson(
+                geojson_data,
+                period="P1M",
+                duration="P1M",
+                transition_time=200,
+                auto_play=True,
+                loop=False,
+                loop_button=True,
+                time_slider_drag_update=True,
+                date_options="YYYY/MM/DD",
+            ).add_to(m)
+
+            folium_static(m)
+
+
+
+
---
+++ helpers.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import boto3
+import streamlit as st
+import pickle 
+from sql_queries import *
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+MASTER_ACCESS_KEY = st.secrets['MASTER_ACCESS_KEY']
+MASTER_SECRET = st.secrets['MASTER_SECRET']
+
+st.cache_data(ttl=60*60*24)
+def grab_pkl(bucket, key):
+    s3 = boto3.client('s3', region_name='us-west-1',
+                aws_access_key_id=MASTER_ACCESS_KEY,
+                aws_secret_access_key=MASTER_SECRET)
+
+    master_accounts_cache = pickle.loads(s3.get_object(Bucket=bucket, Key=key)['Body'].read())
+
+    return master_accounts_cache
+
+## Grabbing all accounts from a list of RDs
+def specific_rds_geocoded(tenants, rds, current, master_accounts):
+    rd_tenants = tenants[tenants['site_code'].isin(rds)]
+    if current:
+        rd_tenants = rd_tenants[rd_tenants['moved_out'] == False]
+
+    # convert rd_tenants['id'] to set for faster membership checking
+    account_ids = set(rd_tenants['id'].values)
+
+    data_list = []
+    for account_id in account_ids:
+        account_data = master_accounts.get(account_id)
+        if account_data is not None:
+            data = {
+                "account_id": account_id,
+                "lat": account_data["location"]["location"]["lat"],
+                "lon": account_data["location"]["location"]["lng"],
+                "full_fips": account_data["census"]["full_fips"],
+                "site_code": rd_tenants.loc[rd_tenants['id'] == account_id, 'site_code'].values[0],
+                "move_in_date": rd_tenants.loc[rd_tenants['id'] == account_id, 'move_in_date'].values[0],
+                "moved_out_at": rd_tenants.loc[rd_tenants['id'] == account_id, 'moved_out_at'].values[0]
+            }
+            data_list.append(data)
+
+    # Convert the list of dictionaries to a DataFrame
+    final_df = pd.DataFrame(data_list)
+
+    # Select the required columns
+    final_df = final_df[['site_code', 'account_id', 'move_in_date', 'moved_out_at', 'lat', 'lon', 'full_fips']]
+
+    return final_df
+
+def generate_date_dict(df):
+    data_dict = {}
+    start_date = pd.Timestamp('2019-05-31')
+    end_date = pd.Timestamp.today().replace(day=1) + relativedelta(months=1) - relativedelta(days=1)  # Last day of current month
+    date_range = pd.date_range(start=start_date, end=end_date, freq='M')
+
+    for _, row in df.iterrows():
+        account_id = row['account_id']
+        move_in_date = row['move_in_date']
+        moved_out_at = row['moved_out_at'] if pd.notnull(row['moved_out_at']) else (end_date + relativedelta(days=1))
+
+        if account_id not in data_dict:
+            data_dict[account_id] = {
+                "site_code": row["site_code"],
+                "lat": row["lat"],
+                "lon": row["lon"],
+                "full_fips": row["full_fips"],
+                "dates": []
+            }
+
+        for date in date_range:
+            if pd.Timestamp(move_in_date) <= pd.Timestamp(date) < pd.Timestamp(moved_out_at):
+                data_dict[account_id]["dates"].append(date)
+
+    return data_dict
+
+
+def dict_to_geojson(data_dict, rd_colors):
+    features = []
+    for account_id, account_data in data_dict.items():
+        for date in account_data["dates"]:
+            dt = datetime.combine(date, datetime.min.time())  # Convert date to datetime
+            timestamp_ms = int(dt.timestamp() * 1000)  # Convert the datetime to timestamp in milliseconds
+            color = rd_colors[account_data["site_code"]]  # Get the color for the RD
+            feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [account_data["lon"], account_data["lat"]],
+                },
+                "properties": {
+                    "site_code": account_data["site_code"],
+                    "account_id": int(account_id),
+                    "timestamp": date.strftime("%Y-%m-%d"),
+                    "times": [timestamp_ms],  # Add times property as a list containing the timestamp in milliseconds
+                    "icon": "circle",
+                    "iconstyle": {
+                        "color": color,
+                        "fillColor": color,
+                        "fillOpacity": 0.7,
+                        "stroke": "false",
+                        "radius": 5
+                    },
+                },
+            }
+            features.append(feature)
+
+    geojson = {"type": "FeatureCollection", "features": features}
+    return geojson
+
+
